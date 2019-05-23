54b823037641ab17f9d1f99702c4cc0778e27313
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 20:25:18 +0530

    Update 27.c

--- 27.c
+++ 27.c
@@ -2,15 +2,15 @@
 #include<ctype.h>
 int main()
 {
-    char a;
-    scanf("%c",&a);
-    if(isdigit(a))
-    {
-    printf("yes");
-    }
-    else
-    {
-    printf("No");
-    }
- return 0;
+char a;
+scanf("%c",&a);
+if(isdigit(a))
+{
+printf("yes");
+}
+else
+{
+ printf("No");
+ }
+return 0;
 }
